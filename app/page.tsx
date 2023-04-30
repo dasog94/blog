547ded509207a0eb61f65df3c52830ba5a5bdc26
jjjj <dasog94@gmail.com>
2023-04-30 18:42:09 +0900

    post 페이지 구성 (static)

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,39 @@
+import fs from "fs"
+import Link from "next/link";
+import matter from "gray-matter"
+import {PostMetadata} from "../components/PostMetadata"
+
+const getPostMetadate = (): PostMetadata[] => {
+    const folder = "posts/"
+    const files = fs.readdirSync(folder)
+    const markdownPosts = files.filter((file) => file.endsWith(".md"))
+
+    const posts = markdownPosts.map((fileName) => {
+        const fileContents = fs.readFileSync(`${folder}${fileName}`, "utf-8")
+        const matterResult = matter(fileContents)
+        return {
+            title: matterResult.data.title,
+            date: matterResult.data.date,
+            subtitle: matterResult.data.subtitle,
+            slug: fileName.replace(".md", "")
+        }
+    })
+
+    return posts
+}
+
 const HomePage = () => {
-    return <h1>Hello from NextJS 13!</h1>
+    const postMetadata = getPostMetadate();
+    const postPreviews = postMetadata.map((post) => (
+        <div key={post.slug}>
+            <Link href={`/posts/${post.slug}`}>
+                <h2>{post.slug}</h2>
+            </Link>
+            <p>{post.subtitle}</p>
+            <p>{post.date}</p>
+        </div>
+    ))
+    return <div>{postPreviews}</div>
 }
 
 export default HomePage
